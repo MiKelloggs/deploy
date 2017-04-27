3262f35fd617909982364fb938ffe87f25cb0f36
MiKelloggs <korologos.michael0@gmail.com>
2017-04-27 05:03:05 -0600

    test

--- hellodb.py
+++ hellodb.py
@@ -139,37 +139,6 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
-class GlassDB:
-
-    def __init__(self):
-        self.connection = sqlite3.connect("glass.db")
-        self.connection.row_factory = dict_factory
-        self.cursor = self.connection.cursor()
-        return
-
-    def __del__(self):
-        self.connection.close()
-        return
-
-    def getGlass(self):
-        self.cursor.execute("SELECT * FROM glassdb")
-        return self.cursor.fetchall()
-
-    def createGlass(self, messages):
-        self.cursor.execute("INSERT INTO glassdb (yearstart, yearend, make, model, partnumber, location, cost, stock) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (messages[0], messages[1], messages[2], messages[3], messages[4], messages[5], messages[6], messages[7]))
-        self.connection.commit()
-        return
-
-    def modifyGlass(self, messages):
-        self.cursor.execute("DELETE FROM glassdb WHERE partnumber = ?",  (messages[4],))
-        self.cursor.execute("INSERT INTO glassdb (yearstart, yearend, make, model, partnumber, location, cost, stock) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (messages[0], messages[1], messages[2], messages[3], messages[4], messages[5], messages[6], messages[7]))
-        self.connection.commit()
-        return
-
-    def deleteGlass(self, data):
-        self.cursor.execute("DELETE FROM glassdb WHERE partnumber = ?",  (data,))
-        self.connection.commit()
-        return
 
 def main():
     db = GlassDB()
